3ae26434173a94d35c1972526d300b9d076e88a3
Sudhir Kumar <sudhir.kumar@credflow.in>
2023-11-26 13:34:24 +0530

    cleaned extra files

---
+++ src/layouts/app-layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const AppLayout = () => {
+    return(
+        <React.Fragment>
+            Hello word
+        </React.Fragment>
+    )
+}
+
+export default AppLayout;
